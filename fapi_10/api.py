8ce9b235bc7657076980f86abf0ef22db9964029
PrinceMalviyaDev <pm306616@gmail.com>
2026-09-23 23:25:28 +0530

    learn HTTP status handling

---
+++ fapi_10/api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, status, HTTPException
+
+app = FastAPI()
+
+@app.post("/users", status_code = status.HTTP_201_CREATED)
+def create_user():
+    return {
+        "message" : "User Created"
+    }
+
+@app.get("/users")
+def get_users():
+    return {
+        "status" : "Success",
+        "message" : "User Fetched",
+        "data" : {
+            "name" : "Prince",
+            "age" : 22
+        }
+    }
+
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    if user_id != 1:
+        raise HTTPException(
+            status_code = 404,
+            detail = "User not found"
+        )
+    return {
+        "id" : 1,
+        "name" : "Prince"
+    }
